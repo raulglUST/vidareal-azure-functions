4e4a29c2fd36aca43edd8209ae65d0c52d2a44e9
Raul Gomez Lopez <Raul.Gomez@ust.com>
2023-06-16 11:41:26 +0200

    update pom properties

--- src/main/java/com/ust/formacion/Upload.java
+++ src/main/java/com/ust/formacion/Upload.java
@@ -14,25 +14,21 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
 /**
  * Azure Functions with HTTP Trigger.
  */
-public class Function {
+public class Upload {
     /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     * This function listens at endpoint "/api/upload". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/upload
+     * 2. curl {your host}/api/upload?name=HTTP%20Query
      */
-    @FunctionName("HttpExample")
+    @FunctionName("upload")
     public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.GET, HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<Optional<String>> request,
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Parse query parameter
-        final String query = request.getQueryParameters().get("name");
-        final String name = request.getBody().orElse(query);
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
 
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
